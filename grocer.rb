5fa39c429dc5e1bcd43c61ceaf8d861a7cca80ba
Sean Beach <sean.beach1@yahoo.com>
2019-12-09 01:16:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,9 +27,20 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0
+  while i < coupons.count do
+    coupon = coupons[i]
+    item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    item_is_in_basket = !!item_with_coupon
+    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+
+    if item_is_in_basket and count_is_big_enough_to_apply
+      apply_coupon_to_cart(item_with_coupon, coupon, cart)
+    end
+    i += 1
+  end
+
+  cart
 end
 
 def apply_clearance(cart)
